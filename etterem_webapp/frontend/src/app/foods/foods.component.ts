e5a452e816ab5cd8dcf77b90ffaaa83d4731ac37
procude <11oconer11@gmail.com>
2019-12-02 09:53:32 +0100

    ételek listázása kategoriankent

--- etterem_webapp/frontend/src/app/foods/foods.component.ts
+++ etterem_webapp/frontend/src/app/foods/foods.component.ts
@@ -13,11 +13,16 @@ import { FoodService, CategoryService } from '../_services';
 export class FoodsComponent implements OnInit {
 
   categories: Category[] = [];
+  foods: Food[] = [];
 
   constructor(private route: ActivatedRoute, private categoryService: CategoryService, private foodService: FoodService, private location: Location ) { }
 
   ngOnInit() {
     this.get_Categories();
+    this.get_Foods();
+  }
+  get_Foods(): void{
+    this.foodService.getFoods().subscribe(foods => this.foods = foods);
   }
 
   get_Categories(): void{
